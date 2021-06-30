3758280508014d922339377bf047ddcc448f9fda
heesu <phis3210@naver.com>
2021-06-30 16:04:39 +0900

    search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,24 @@
-import React from "react";
-
-const SearchBar = () => {
-  return <input />;
-};
+import React, { Component } from "react";
+// === const Component = React.Component;
 
 
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { term: '' };
+  }
+  render() {
+    return (
+    <div className="search-bar">
+      <input 
+      value = {this.state.term}
+      onChange={event => this.setState({ term: event.target.value })} />
+    </div>
+    );
+  }
+
+}
 
 export default SearchBar;
 
